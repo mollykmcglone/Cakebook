4dd3edd25909b0f3953b10ebdde859d87951c563
Molly McGlone <mollykmcglone@gmail.com>
2016-03-21 21:25:58 -0700

    finish debugging javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,26 @@ function Bakery(bakery, website) {
   this.cakes = [];
 }
 //function constructor to create all the cakes//
-function Cake(name, erikFlavor, erikTexture, mollyFlavor, mollyTexture) {
-  this.name = name;
-  this.erikFlavor = erikFlavor;
-  this.erikTexture = erikTexture;
-  this.mollyFlavor = mollyFlavor;
-  this.mollyTexture= mollyTexture;
+function Cake(cakeName, erikScore, mollyScore) {
+  this.cakeName = cakeName;
+  this.erikScore = erikScore;
+  this.mollyScore = mollyScore;
 }
 // prototype for Cakes that creates a summary statement of our rankings for each cake//
 Cake.prototype.summary = function() {
-  return this.name + ": Erik ranks " + this.erikFlavor + "/" + this.erikTexture + " and Molly ranks " + this.mollyFlavor + "/" + this.mollyTexture;
+  return this.cakeName + ": Erik ranks " + this.erikScore + " and Molly ranks " + this.mollyScore;
 }
 //function that resets all the fields on the input form//
 function resetFields() {
     $("input#new-bakery-name").val("");
     $("input#new-bakery-website").val("");
     $("input.new-cake-name").val("");
-    $("input.new-Erik-flavor-score").val("");
-    $("input.new-Erik-texture-score").val("");
-    $("input.new-Molly-flavor-score").val("");
-    $("input.new-Molly-texture-score").val("");
+    $("input.new-Erik-score").val("");
+    $("input.new-Molly-score").val("");
+}
+
+function removeDiv(divId) {
+  $("#" +divId).remove();
 }
 
 // user interface logic
@@ -33,28 +33,22 @@ $(document).ready(function() {
 //upon click of the "add cake" button...//
   $("#add-cake").click(function() {
     //to the new-cakes ID, add...//
-    $("#new-cakes").append('<div class="new-cake">' +
+    $("#new-cakes").append('<div id="additional">'+
+                              '<div class="new-cake">' +
                                  '<div class="form-group">' +
-                                   '<label for="new-cake">Cake</label>' +
-                                   '<input type="text" class="form-control new-cake">' +
+                                   '<label for="new-cake-name">Name of Cake</label>' +
+                                   '<input type="text" class="form-control new-cake-name">' +
                                  '</div>' +
                                  '<div class="form-group">' +
-                                   '<label for="new-Erik-flavor-score">Erik Flavor Score</label>' +
-                                   '<input type="text" class="form-control new-Erik-flavor-score">' +
+                                   '<label for="new-Erik-score">Erik Flavor/Texture Score</label>' +
+                                   '<input type="text" class="form-control new-Erik-score">' +
                                  '</div>' +
                                  '<div class="form-group">' +
-                                   '<label for="new-Erik Texture Score">Erik Texture Score</label>' +
-                                   '<input type="text" class="form-control new-Erik-texture-score">' +
+                                   '<label for="new-Molly-score">Molly Flavor/Texture Score</label>' +
+                                   '<input type="text" class="form-control new-Molly-score">' +
                                  '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-Molly-flavor-score">Molly Flavor Score</label>' +
-                                   '<input type="text" class="form-control new-Molly-flavor-score">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-Molly Texture Score">Molly Texture Score</label>' +
-                                   '<input type="text" class="form-control new-Molly-texture-score">' +
-                                 '</div>' +
-                               '</div>');
+                              '</div>' +
+                            '</div>');
   });
 
   $("form#new-bakery").submit(function(event) {
@@ -65,20 +59,19 @@ $(document).ready(function() {
     var newBakery = new Bakery(inputtedBakeryName, inputtedBakeryWebsite);
 
     $(".new-cake").each(function() {
-      var inputtedCake = $(this).find("input.new-cake").val();
-      var inputtedErikFlavorScore = $(this).find("input.new-Erik-flavor-score").val();
-      var inputtedErikTextureScore = $(this).find("input.new-Erik-texture-score").val();
-      var inputtedMollyFlavorScore = $(this).find("input.new-Molly-flavor-score").val();
-      var inputtedMollyTextureScore = $(this).find("input.new-Molly-texture-score").val();
-      var newCake = new Cake(inputtedCake, inputtedErikFlavorScore, inputtedErikTextureScore, inputtedMollyFlavorScore, inputtedMollyTextureScore);
+      var inputtedCake = $(this).find("input.new-cake-name").val();
+      var inputtedErikScore = $(this).find("input.new-Erik-score").val();
+      var inputtedMollyScore = $(this).find("input.new-Molly-score").val();
+      var newCake = new Cake(inputtedCake, inputtedErikScore, inputtedMollyScore)
       newBakery.cakes.push(newCake)
     });
 
-    $("ul#bakeries").append("<li><span class='bakery'>" + newBakery + "</span></li>");
+    $("ul#bakeries").append("<li><span class='bakery'>" + newBakery.bakeryName + "</span></li>");
 
     $(".bakery").last().click(function() {
       $("#show-bakery").show();
-      $("#show-bakery h2").text(newBakery);
+      $("#show-bakery h2").text(newBakery.bakeryName);
+      $(".bakery-name").text(newBakery.bakeryName);
       $(".bakery-website").text(newBakery.website);
       $("ul#cakes").text("");
       newBakery.cakes.forEach(function(cake) {
@@ -87,6 +80,7 @@ $(document).ready(function() {
     });
 
     resetFields();
+    removeDiv("additional");
 
   });
 });
